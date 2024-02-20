5be65be52d8bb8e2d29551bf912710bca337ae19
Carlaurence <ingeniero.indbermeo@gmail.com>
2024-02-19 22:18:51 -0500

    ligero cambio en el file db.js

--- config/db.js
+++ config/db.js
@@ -4,17 +4,21 @@ mongoose.set('strictQuery', true)
 
 const connectDB = async () => {
     try{
-        const connection = await mongoose.connect(
-            `${dbConfig.uri}${dbConfig.name}`, { useNewUrlParser: true, useUnifiedTopology: true }
-            )
-            const url = `${connection.connection.host}:${connection.connection.port}`;
-            //console.log(`MongoDB Conectado en :${url}`);
-            //console.log(`MongoDB Conectado en : ${dbConfig.uri}${dbConfig.name}`);
-            console.log('MongoDB Ecommerce conectado...')
+        const stringConnection = await mongoose.connect(
+            `${dbConfig.uri}${dbConfig.name}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+
+        const url = `${stringConnection.connection.host}:${stringConnection.connection.port}`;
+        //console.log(`MongoDB Conectado en :${url}`);
+        //console.log(`MongoDB Conectado en : ${dbConfig.uri}${dbConfig.name}`);
+        console.log('MongoDB Multicomerciales_V2 conectado...')
+
     }catch(error){
-        console.log(`error: ${error.message}`)
-        process.exit(1)
-    
+        console.log(`error : ${error.message}`);
+        process.exit(1);
     }
 }
 module.exports = connectDB;
